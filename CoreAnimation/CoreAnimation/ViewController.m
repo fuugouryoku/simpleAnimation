88896cd1b935693a338c526fcdb47e6ad5a0889a
fuugouryoku <qianfeng@qianfengdemac-mini-24.local>
2016-09-18 11:07:36 +0800

    20160918

--- CoreAnimation/CoreAnimation/ViewController.m
+++ CoreAnimation/CoreAnimation/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self keyframeAnimationRound];
     
 }
 
@@ -25,17 +26,53 @@
     // Dispose of any resources that can be recreated.
 }
 //关键帧动画，将动画中指定的帧作为节点，由core animation自动补全动画
+
+-(void)keyframeAnimationRound
+{
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(50, 100, 42, 81)];
+    [self.view addSubview:imageView];
+    imageView.image = [UIImage imageNamed:@"satellite"];
+    
+    CAKeyframeAnimation *keyframeAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 100, 250, 250)];
+    keyframeAnimation.path = bezierPath.CGPath;
+    keyframeAnimation.duration = 4;
+    keyframeAnimation.additive = YES;
+    //重复次数无穷
+    keyframeAnimation.repeatDuration = HUGE_VALF;
+    keyframeAnimation.calculationMode = kCAAnimationPaced;
+    //自选转
+    keyframeAnimation.rotationMode = kCAAnimationRotateAuto;
+    
+    [imageView.layer addAnimation:keyframeAnimation forKey:@"keyframeAnimationRound"];
+}
+
 -(void)keyframeAnimation
 {
+    
+    UIBezierPath* bezierPath = [UIBezierPath bezierPath];
+    [bezierPath moveToPoint: CGPointMake(25.5, 15.5)];
+    [bezierPath addCurveToPoint: CGPointMake(25.5, 82.5) controlPoint1: CGPointMake(224.5, 26.5) controlPoint2: CGPointMake(25.5, 82.5)];
+
     UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(100, 200, 200, 40)];
     textField.secureTextEntry = YES;
     textField.layer.borderColor = [UIColor grayColor].CGColor;
     textField.layer.borderWidth =1;
     [self.view addSubview:textField];
     
-    CAKeyframeAnimation *keyframeAnimation = [CAKeyframeAnimation animation];
+    CAKeyframeAnimation *keyframeAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//    keyframeAnimation.values = @[@0,@10,@-10,@10,@0];
+    keyframeAnimation.repeatDuration = 1;
+//    keyframeAnimation.keyTimes = @[@0,@(1/6.0),@(2/6.0),@(3/6.0),@(4/6.0)];
     
+    keyframeAnimation.path = bezierPath.CGPath;
     
+    keyframeAnimation.duration = (3/6.0);
+    //设置动画可添加
+    keyframeAnimation.additive = YES;
+    [textField.layer addAnimation:keyframeAnimation forKey:@"CAKeyframeAnimation"];
+
 }
 
 -(void)basicAnimation
