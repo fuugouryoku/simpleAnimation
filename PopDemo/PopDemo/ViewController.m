020671e9e4ce0aaf56d96b310eedbe556653bbfa
fuugouryoku <qianfeng@qianfengdeMac-mini.local>
2016-09-18 14:59:44 +0800

    fuu
    20160918

--- PopDemo/PopDemo/ViewController.m
+++ PopDemo/PopDemo/ViewController.m
@@ -8,11 +8,34 @@
 
 #import "ViewController.h"
 #import "TableViewCell.h"
+#import <POP.h>
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    [self decayAnimation];
+}
+
+//阻尼效果
+-(void)decayAnimation
+{
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(20, 100, 100, 100)];
+    [self.view addSubview:view];
+    view.backgroundColor = [UIColor redColor];
+    
+    POPDecayAnimation *decayAnimation = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPositionX];
+    //初始值
+    decayAnimation.velocity = @400;
+    
+    [view.layer pop_addAnimation:decayAnimation forKey:@"decayAnimation"];
+    
+}
+
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return 3;
